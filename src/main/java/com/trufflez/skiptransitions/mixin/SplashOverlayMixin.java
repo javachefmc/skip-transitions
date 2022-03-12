4c336d14993b58f7551e7b85e49f4cfcec0cfad0
TrufflezMC <11602232+trufflezmc@users.noreply.github.com>
2022-03-12 01:47:55 -0500

    Mojang logo fade in

--- src/main/java/com/trufflez/skiptransitions/mixin/SplashOverlayMixin.java
+++ src/main/java/com/trufflez/skiptransitions/mixin/SplashOverlayMixin.java
@@ -1,19 +1,15 @@
 package com.trufflez.skiptransitions.mixin;
 
-import com.trufflez.skiptransitions.SkipTransitions;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.SplashOverlay;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.resource.ResourceReload;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Optional;
-import java.util.function.Consumer;
 
 @Mixin(SplashOverlay.class)
 public class SplashOverlayMixin {
@@ -21,20 +17,29 @@ public class SplashOverlayMixin {
     
     Splash on startup
     Change resourcepack
-    - Mojang logo fade out covers up buttons
-    TODO: Mojang logo fade in
-    - I really don't like the fades  
+    - Splash screen fade out covers up buttons
+    - Removes fade in for both background and logo
     
      */
 
-	@Shadow @Final private MinecraftClient client;	// Access the client
-	@Shadow private long reloadCompleteTime;		// Checks when to do what
+	@Mutable @Shadow @Final private MinecraftClient client;	// Access the client
+	@Shadow private long reloadCompleteTime;				// Checks when to do what
 	
-	@Inject(at = @At("HEAD"), method = "render")
-	public void render(CallbackInfo ci) { 
+	@ModifyVariable(method = "withAlpha", at = @At("HEAD"), ordinal = 1, argsOnly = true)
+	private static int alpha(int value) {
+		return 255; // This method gets called for determining background color transparency
+	}
+	
+	@ModifyVariable(method = "render", at = @At("STORE"), name = "h")
+	private float h(float value) {
+		return 1.0f; // This parameter is used for the Mojang logo transparency
+	}
+	
+	@Inject(method = "render", at = @At("HEAD"))
+	public void render2(CallbackInfo ci) {
+
 		if (this.reloadCompleteTime > 1) {
-			this.client.setOverlay(null); // Only remove the Mojang overlay when it's unnecessary
+			this.client.setOverlay(null); // Only remove the overlay when it's unnecessary
 		}
-		SkipTransitions.LOGGER.debug("Splash screen mixin injected");
 	}
 }
